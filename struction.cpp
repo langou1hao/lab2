29e757d25aff770e06f4039a4d60c4a7ceabc800
langou1hao <90843942+langou1hao@users.noreply.github.com>
2021-09-22 10:16:37 +0800

    Add files via upload

---
+++ struction.cpp
@@ -0,0 +1,9 @@
+
+struct countdata{
+    int cttotal = 0;    //×ÜºÍ
+    int ctsw = 0;       //switch
+    int ctcase[1000] = {0};     //cast
+    int ctie = 0;       //if-else
+    int ctieie = 0;     //if-else if-else
+};
+countdata result;
